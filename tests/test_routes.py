48e1df8daedcd56013e7cda5ba33a9ffa54ecec0
samuelhacks22 <samuelvasquezdiaz2@gmail.com>
2025-03-30 18:02:03 -0400

    Refactor Dockerfile for Python environment setup and add CI configuration with Makefile

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,3 +171,17 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
